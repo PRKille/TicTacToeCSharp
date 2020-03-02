493f4ebefa391833ebc4acf327ffcab493e8ec67
Jamison Cozart and Patrick Kille <jamison@>
2020-03-02 13:30:20 -0800

    Test for initiation of game and printing board

--- TicTacToe.Tests/ModelTests/TicTacToeTests.cs
+++ TicTacToe.Tests/ModelTests/TicTacToeTests.cs
@@ -7,10 +7,40 @@ namespace TicTacToe.TestTools
   public class TicTacToeTests
   {
     [TestMethod]
-    public void Game_IntsantiatesWithCorrectValues_Game()
+    public void Game_IntsantiatesWithCorrectValues_Player()
     {
       Game newGame = new Game();
-      Assert.AreEqual('O', newGame.CurrentPlayer);
+      Assert.AreEqual('X', newGame.CurrentPlayer);
+    }
+    
+    [TestMethod]
+    public void Game_IntsantiatesWithCorrectValues_TurnCount()
+    {
+      Game newGame = new Game();
+      Assert.AreEqual(1, newGame.TurnCount);
+    }
+    
+    [TestMethod]
+    public void Game_IntsantiatesWithCorrectValues_Board0()
+    {
+      Game newGame = new Game();
+      char[] BoardArr = {' ', ' ', ' '};
+      CollectionAssert.AreEqual(BoardArr, newGame.Board[0]);
+    }
+
+    [TestMethod]
+    public void PrintBoard_ConstructsBoardCorrectly_Board()
+    {
+      Game newGame = new Game();
+      string newBoard = $" | | \n-----\n | | \n-----\n | | \n";
+      Assert.AreEqual(newBoard, newGame.PrintBoard());
+    }
+    [TestMethod]
+    public void IncrementTurn_Add1ToTurn_2()
+    {
+      Game newGame = new Game();
+      newGame.IncrementTurn();
+      Assert.AreEqual(2, newGame.TurnCount);
     }
   }
 }
--- TicTacToe/Models/TicTacToe.cs
+++ TicTacToe/Models/TicTacToe.cs
@@ -17,5 +17,15 @@ namespace TicTacToe
       Board[1] = new char[] {' ', ' ', ' '};
       Board[2] = new char[] {' ', ' ', ' '};
     }
+
+    public string PrintBoard()
+    {
+      return $"{Board[0][0]}|{Board[0][2]}|{Board[0][2]}\n-----\n{Board[1][0]}|{Board[1][2]}|{Board[1][2]}\n-----\n{Board[2][0]}|{Board[2][2]}|{Board[2][2]}\n";
+    }
+
+    public void IncrementTurn()
+    {
+      TurnCount++;
+    }
   }
 }
